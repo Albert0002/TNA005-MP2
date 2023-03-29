72ed60df8be120e846a5632b36e039c57844d1b3
Albert0002 <albert.lannerstrom@gmail.com>
2023-03-29 10:02:50 +0200

    main

---
+++ data-mp2/main.m
@@ -0,0 +1,10 @@
+load("uspsDigits.mat"); % Load data
+
+trainingData = reshape(trainDigits, [256,7291]);    % Reshapes training data from 3D-matrix to 2D. Column by column (each 16x16 becomes 256x1)
+testData = reshape(testDigits, [256,2007]);         % Reshapes test data from 3D-matrix to 2D. Column by column (each 16x16 becomes 256x1)
+
+x = testData(:,1)                                   % Fetch first digit from testData
+
+y = trainingData(:,1)
+
+norm(x-y)
