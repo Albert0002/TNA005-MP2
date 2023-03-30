e9cdb77f743b4a6c11f140d7194de6df72e29644
Albert0002 <albert.lannerstrom@gmail.com>
2023-03-30 09:11:00 +0200

    First test complete

--- data-mp2/nearest_neighbor.m
+++ data-mp2/nearest_neighbor.m
@@ -6,8 +6,27 @@ trainingData = reshape(trainDigits, [256,7291]);    % Reshapes training data fro
 %% Tests
 testData = reshape(testDigits, [256,2007]);         % Reshapes test data from 3D-matrix to 2D. Column by column (each 16x16 becomes 256x1)
 
-x = testData(:,1)                                   % Fetch first digit from testData
+x = testData(:,1);                                  % Fetch first digit from testData
 
-y = trainingData(:,1)
+y = trainingData(:,1);                              % Fetch first digit from trainingData
+y_result = y;
 
-norm(x-y)
+n = length(trainingData);
+norm_results = zeros(n,1);                          % Allocate norm_results
+
+shortest_norm = norm(x-y);                          % Check first digit/vector
+norm_results(1) = shortest_norm;                    % Add result to list
+y_index = 1;
+
+for i = 2:n
+    y = trainingData(:,i);
+    temp_norm = norm(x-y);
+    norm_results(i) = temp_norm;
+    if temp_norm < shortest_norm
+        shortest_norm = temp_norm;
+        y_result = y;
+        y_index = i;
+    end
+end
+
+result = trainAns(y_index)
