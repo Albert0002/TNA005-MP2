5a0332c571d55db8bca53de883288dda92f974a8
SamuelHellqvist <samuel@hellqvist.nu>
2023-03-30 10:23:11 +0200

    new things in the centroid method
    now we can create cool models

--- data-mp2/centroid.m
+++ data-mp2/centroid.m
@@ -5,17 +5,11 @@ trainingData = reshape(trainDigits, [256,7291]);    % Reshapes training data fro
 
 %% Tests
 testData = reshape(testDigits, [256,2007]);         % Reshapes test data from 3D-matrix to 2D. Column by column (each 16x16 becomes 256x1)
-n = length(testData);
 
-%finding a model for 0
-foundDigits = [];
-for i = 1:n
-    if trainAns(i) == 0
-        foundDigits(:, end+1) = trainingData(:, i);
-    end
+v = zeros(256, 10);
+
+for i = 1:10
+    v(:,i) = create_model(i-1, trainingData, trainAns, testData);
 end
-
-foundDigits = foundDigits';
-modelForDigit = mean(foundDigits);
-modelForDigit = reshape(modelForDigit, [16, 16]);
-ima(modelForDigit);
+number = 7;
+ima(reshape(v(:,number+1), [16, 16]));
--- data-mp2/create_model.m
+++ data-mp2/create_model.m
@@ -1,15 +1,13 @@
-function modelForDigit = create_model(x, trainingData, trainAns)
-n = length(testData);
-
-%finding a model for 0
-foundDigits = [];
-for i = 1:n
-    if trainAns(i) == x
-        foundDigits(:, end+1) = trainingData(:, i);
+function modelForDigit = create_model(x, trainingData, trainAns, testData)
+    n = length(testData);
+    
+    %finding a model for 0
+    foundDigits = [];
+    for i = 1:n
+        if trainAns(i) == x
+            foundDigits(:, end+1) = trainingData(:, i);
+        end
     end
-end
-
-
-modelForDigit = mean(foundDigits');
+    modelForDigit = mean(foundDigits, 2)
 end
 
