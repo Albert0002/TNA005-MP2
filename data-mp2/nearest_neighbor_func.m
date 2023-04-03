e2e85d5f2f2afa6cc14af8e90a330d0738ac4a7f
Majsburken <annabark1@gmail.com>
2023-04-03 10:47:26 +0200

    Comments nearest_neighbour_func

--- data-mp2/nearest_neighbor_func.m
+++ data-mp2/nearest_neighbor_func.m
@@ -1,25 +1,29 @@
 function [y_result, digit, shortest_norm] = nearest_neighbor_func(x, data, answer)
 
     y = data(:,1);                              % Fetch first digit from trainingData
-    y_result = y;
+    y_result = y;                               % Sets a first value
     
-    n = size(data, 2);
+    n = size(data, 2);                          % Number of columns
     norm_results = zeros(n,1);                          % Allocate norm_results
     
-    shortest_norm = norm(x-y);                          % Check first digit/vector
+    shortest_norm = norm(x-y);                          % Check first digit/vector distance
     norm_results(1) = shortest_norm;                    % Add result to list
-    y_index = 1;
+    y_index = 1;                                        % Index of first digit
     
-    for i = 2:n
+    for i = 2:n                                         % Loop through all training data
         y = data(:,i);
-        temp_norm = norm(x-y);
+        temp_norm = norm(x-y);                          % Calculate norm
         norm_results(i) = temp_norm;
-        if temp_norm < shortest_norm
+        if temp_norm < shortest_norm                    % If temp < shortest, sets new shortest, sets result
             shortest_norm = temp_norm;
-            y_result = y;
-            y_index = i;
+            y_result = y;                               % Nearest digit vector
+            y_index = i;                                % Index for nearest digit vector
         end
     end
     
+<<<<<<< Updated upstream
     digit = answer(y_index);
+=======
+    digit = answer(y_index);                            % The nearest digit
+>>>>>>> Stashed changes
 end
